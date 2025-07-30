51d09e2e556b26d13c255633c885795df825d862
Adam <adamsepp@web.de>
2025-07-30 12:15:38 +0200

    Add initial GoogleTest setup and basic logger test
    - Integrated GoogleTest using FetchContent
    - Added LoggerTest.cpp with first test case verifying order of log entries
    - Linked fmt::fmt to test target to resolve include errors
    - Ensured logger buffer access is thread-safe and accessible in tests

--- Tests/LoggerTest.cpp
+++ Tests/LoggerTest.cpp
@@ -1,7 +1,19 @@
-#include <iostream>
+#include <gtest/gtest.h>
+#include "Logger/Logger.h"
 
-int main()
+TEST(LoggerTest, LogsAreStoredInOrder)
 {
-    std::cout << "No tests exists yet. Later we will integrate gTest and add some real tests here." << std::endl;
-    return 0;
+    Logger::Log(LogLevel::Info, "Test log {}", 1);
+    Logger::Log(LogLevel::Warning, "Test log {}", 2);
+
+    const auto& buffer = Logger::GetBuffer();
+    const size_t readIndex = Logger::GetReadIndex();
+    const size_t size = Logger::GetSize();
+
+    ASSERT_GE(size, 2);
+    const auto& msg1 = buffer[(readIndex + size - 2) % buffer.size()];
+    const auto& msg2 = buffer[(readIndex + size - 1) % buffer.size()];
+
+    EXPECT_EQ(msg1.level, LogLevel::Info);
+    EXPECT_EQ(msg2.level, LogLevel::Warning);
 }
